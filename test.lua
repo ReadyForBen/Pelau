fa90ea3cf330304987e653928a60998cf55fa50f
ReadyForBen <77867769+ReadyForBen@users.noreply.github.com>
2024-08-05 19:51:01 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,1 @@
-local TextChatService = game:GetService("TextChatService")
-
-local tag = "[ALERT]"
-local message = "hey test"  -- The message you want to send
-
--- Send the system message to the default text channel (RBXGeneral)
-TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage(tag .. " " .. message)
+local TextChatService = game:GetService("TextChatService") local tag = "[ALERT]" local message = "hey test" TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage(tag .. " " .. message)
