2349b61f78ae09c0df6b0febf2fb3a2a69e73c76
ReadyForBen <77867769+ReadyForBen@users.noreply.github.com>
2024-08-05 20:02:21 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,19 @@
-local TextChatService = game:GetService("TextChatService") local tag = "[ALERT]" local message = "hey test" TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage(tag .. " " .. message)
+local TextChatService = game:GetService("TextChatService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local AlertEvent = ReplicatedStorage:WaitForChild("ServerAlerts")
+
+AlertEvent.OnClientEvent:Connect(function()
+    task.wait(3)
+    local tag = "[ALERT]"
+    local message = "hello test"  -- Static message to be displayed
+
+    -- Ensure RBXSystem channel exists
+    local systemChannel = TextChatService.TextChannels:FindFirstChild("RBXSystem")
+
+    if systemChannel then
+        -- Send the system message to the RBXSystem text channel
+        systemChannel:SendAsync(tag .. " " .. message)
+    else
+        warn("RBXSystem channel not found!")
+    end
+end)
