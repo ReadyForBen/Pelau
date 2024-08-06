27dd3296e262ca33e1fb8fc21fd9036faba87b23
ReadyForBen <77867769+ReadyForBen@users.noreply.github.com>
2024-08-05 20:22:19 -0500

    Update Secure.lua

--- Secure.lua
+++ Secure.lua
@@ -1,11 +1,12 @@
 task.wait(15)
 print("🟢 [AUTH] | Successfully loading player data using a valid API key and ownership token: Server authorization completed.")
 
-
-
 local Players = game:GetService("Players")
+
 local function kickAllPlayers(reason)
     for _, player in ipairs(Players:GetPlayers()) do
         player:Kick(reason)
     end
+end
+
 kickAllPlayers("⚠️ Pealu's internal dashboard returned a 404 error. Please notify a developer. You were kicked because your data failed to load, and server bans, logs, etc., are currently unavailable.")
