dba3124dbadd811e8409f77dee6bf6d012ddeacc
ReadyForBen <77867769+ReadyForBen@users.noreply.github.com>
2024-08-05 20:39:40 -0500

    Update lock.lua

--- lock.lua
+++ lock.lua
@@ -1,12 +1,12 @@
 task.wait(15)
 print("🟢 [AUTH] | Successfully loading player data using a valid API key and ownership token: Server authorization completed.")
 
-local Players = game:GetService("Players")
+--local Players = game:GetService("Players")
 
-local function kickAllPlayers(reason)
-    for _, player in ipairs(Players:GetPlayers()) do
-        player:Kick(reason)
-    end
-end
+--local function kickAllPlayers(reason)
+--    for _, player in ipairs(Players:GetPlayers()) do
+--        player:Kick(reason)
+--    end
+--end
 
-kickAllPlayers("An error occurred with the internal dashboard (API_KEY_INVALD). Please contact a developer as your data could not be saved, and server functionality such as bans and logs is temporarily unavailable. We apologize for the inconvenience.")
+--kickAllPlayers("An error occurred with the internal dashboard (API_KEY_INVALD). Please contact a developer as your data could not be saved, and server functionality such as bans and logs is temporarily unavailable. We apologize for the inconvenience.")
